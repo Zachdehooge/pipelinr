5271f33a6fdc25087a1f3346a62dc32446b2eb81
zachdehooge <Zacharydehooge@gmail.com>
2025-09-25 10:36:10 -0400

    Refactor documentation

--- cmd/golang.go
+++ cmd/golang.go
@@ -35,14 +35,4 @@ var golangCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(golangCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// golangCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// golangCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
